8796bf6bae3273d0d6cc727e5a47284e13cb8370
Evan Barbeyto <evann.bar@gmail.com>
2024-02-22 15:55:23 -0800

    replace menu link with insta

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const food = document.querySelector('.food');
 const dialog = document.querySelector('dialog');
 
 let stores = [
-    {name: "mango mango", image: "./images/mangomango.jpg", menu: "https://www.mangomangodessert.com/menu"},
-    {name: "zippy's", image: "http://tinyurl.com/23de4unp", menu: "https://www.zippys.com/lasvegas/"},
-    {name: "ton shou", image: "http://tinyurl.com/22zob9hv", menu: "https://www.yelp.com/biz/ton-shou-las-vegas"},
-    {name: "purple potato", image: "http://tinyurl.com/242lhv4y", menu: "http://tinyurl.com/279zvzaj"}
+    {name: "mango mango", image: "https://tinyurl.com/29yfg3aq", menu: "https://www.instagram.com/mangomango.lasvegas/"},
+    {name: "zippy's", image: "http://tinyurl.com/23de4unp", menu: "https://www.instagram.com/zippys/"},
+    {name: "ton shou", image: "http://tinyurl.com/22zob9hv", menu: "https://www.instagram.com/tonshoulv/"},
+    {name: "purple potato", image: "http://tinyurl.com/242lhv4y", menu: "https://www.instagram.com/purplepotatolv/"}
 
 ]
 //objects use for...in vs arrays who use for...of
@@ -49,12 +49,8 @@ const close = document.querySelector('.close')
 close.addEventListener('click', () => {
     dialog.close();
 })
+
 const heart = document.querySelector(".heart");
-heart.addEventListener('dblclick', () => {
-    if(heart.classList.contains('clicked')) {
-        heart.classList.remove('.clicked')
-    } else {
-        heart.classList.toggle('clicked');
-        heart.innerHTML ='<img src ="./images/heart_filled.png">'
-    }
+heart.addEventListener('click', () => {
+    heart.innerHTML = (heart.classList.toggle('clicked')) ? '<img src ="./images/heart_filled.png">' : '<img src ="./images/heart_empty.png">';
 });
