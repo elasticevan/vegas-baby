0c32b250795a4fbddc61aca9eebb4851339f2b82
Evan Barbeyto <evann.bar@gmail.com>
2024-04-02 23:59:10 -0700

    added couple chi restys

--- script.js
+++ script.js
@@ -70,7 +70,9 @@ let stores = [
     {city: "vegas", name: "hui lao shan", image: "https://tinyurl.com/2brnm3yp", insta: "https://www.instagram.com/huilaushanlv/?hl=en", location: "https://tinyurl.com/23kcvand"},
     {city: "vegas", name: "tora katsu", image: "https://tinyurl.com/24r9nua9", insta: "https://www.instagram.com/torakatsulv/", location: "https://tinyurl.com/22tmt7k3"},
     //chicago
-    {city: "chicago", name: "the bear", image: "https://tinyurl.com/2c9ba2bh", insta: "https://www.instagram.com/mr.beefchicago/", location: "https://tinyurl.com/27ux5qaq"},
+    {city: "chicago", name: "mr. beef", image: "https://tinyurl.com/2c9ba2bh", insta: "https://www.instagram.com/mr.beefchicago/", location: "https://tinyurl.com/27ux5qaq"},
+    {city: "chicago", name: "gretel", image: "https://tinyurl.com/2dbb9jub", insta: "https://www.instagram.com/gretelchicago/?hl=en", location: "https://tinyurl.com/24ssmxpt"},
+    {city: "chicago", name: "ricobene's", image: "https://tinyurl.com/25qwlfzj", insta: "https://www.instagram.com/ricobenes/", location: "https://tinyurl.com/22wthuxq"},
 ]
 stores.forEach(store => store.favorite = 'no');
 stores.sort((a, b) => a.name.localeCompare(b.name));
