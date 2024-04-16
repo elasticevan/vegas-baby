8150848e161bcd624c9a7b4987d91db227388798
Evan Barbeyto <evann.bar@gmail.com>
2024-04-15 20:55:03 -0700

    test

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ let stores = [
     {city: "chicago", name: "girl & goat", image:  "https://tinyurl.com/2ap5gzjb", insta: "https://www.instagram.com/girl.and.the.goat/?hl=en", location: "https://tinyurl.com/297oknh4"},
     {city: "chicago", name: "cleo's southern kitchen", image:  "https://tinyurl.com/2btj7yu7", insta: "https://www.instagram.com/cleos_southerncuisine/?hl=en", location: "https://tinyurl.com/27zhp2t6"},
     {city: "chicago", name: "hero coffee", image:  "https://tinyurl.com/26yywldu", insta: "https://www.instagram.com/herocoffee/?hl=en", location: "https://tinyurl.com/2bxcnclp"},
+    {city: "chicago", name: "hero coffee", image:  "https://tinyurl.com/26yywldu", insta: "https://www.instagram.com/herocoffee/?hl=en", location: "https://tinyurl.com/2bxcnclp"},
 ]
 
 stores.forEach(store => store.favorite = 'no');
